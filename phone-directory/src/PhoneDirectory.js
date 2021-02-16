6e8841ee3b08ee4a318713885ca9b593b9a6f968
shrimanwar92 <shrimanwar92@gmail.com>
2021-02-16 17:35:50 +0530

    modify route file

--- phone-directory/src/PhoneDirectory.js
+++ phone-directory/src/PhoneDirectory.js
@@ -1,8 +1,60 @@
-import { Component } from "react";
+import { useState } from "react";
 import AddSubscriber from "./AddSubscriber";
 import ShowSubscribers from "./ShowSubscribers";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+export default function PhoneDirectory() {
+    const [state, setState] = useState({
+        subscribersList: [
+            {
+                id: 1,
+                name : "Prahalad",
+                phone: "123456"
+            },
+            {
+                id: 2,
+                name: "Rekha",
+                phone: "11111"
+            }
+        ]
+    });
+
+    const addSubscriberHandler = (newSubscriber) => {
+        let subscribersList = state.subscribersList;
+        if (subscribersList.length > 0) {
+            newSubscriber.id = subscribersList[subscribersList.length - 1].id + 1;
+        } else {
+            newSubscriber.id = 1;
+        }
+        subscribersList.push(newSubscriber);
+        setState({ subscribersList: subscribersList });
+    }
+
+    const deleteSubscriberHandler = (subscriberId) => {
+        let subscriberList = state.subscribersList;
+        let subscriberIndex = 0 ;
+        subscriberList.forEach(function(subscriber, index) {
+            if(subscriber.id === subscriberId) {
+                subscriberIndex = index;
+            }
+        });
+        let newSubscriberList = subscriberList;
+        newSubscriberList.splice(subscriberIndex, 1);
+        setState({subscribersList: newSubscriberList});
+    }
+
+    return (
+        <Router>
+                <Route exact path='/' render={(props) => <ShowSubscribers {...props}
+                                                                          subscribersList={state.subscribersList}
+                                                                          deleteSubscriberHandler={deleteSubscriberHandler}
+                /> } />
+                <Route exact path='/add' render={({history}, props) => <AddSubscriber history={history} {...props} addSubscriberHandler={addSubscriberHandler} /> } />
+        </Router>
+    );
+}
+
+/*
 class PhoneDirectory extends Component {
 
     constructor() {
@@ -62,4 +114,4 @@ class PhoneDirectory extends Component {
     }
 }
 
-export default PhoneDirectory;
+export default PhoneDirectory;*/
